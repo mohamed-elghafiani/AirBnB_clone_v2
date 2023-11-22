abd898031c292130491c7a694dbedfef65583b5b
Meriem matnoussi <meriem.matnoussi123@gmail.com>
2023-11-22 21:27:56 +0000

    task 7

--- models/user.py
+++ models/user.py
@@ -2,6 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
