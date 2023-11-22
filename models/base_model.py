beecde560722ccb56bf9ac7a68b10fafaab4d5bc
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-11-22 15:51:52 -0800

    fixed import storage

--- models/base_model.py
+++ models/base_model.py
@@ -41,7 +41,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        sstorate.new(self)
+        storate.new(self)
         storage.save()
 
     def to_dict(self):
