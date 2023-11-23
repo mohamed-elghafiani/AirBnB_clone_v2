481d448361b5bbf8ff94d7995055c06c546a21f8
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-11-22 18:21:05 -0800

    DBStorage - Place

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
+from models.place import Place
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -12,4 +13,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
+    places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
