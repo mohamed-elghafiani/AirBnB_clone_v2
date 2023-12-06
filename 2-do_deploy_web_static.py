d81968243521261ec30863a17d8d9bcbe90ad596
meriem matnoussi <meriem.matnoussi123@gmail.com>
2023-12-06 05:32:25 -0800

    task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script to deploy web static"""
+from fabric.api import *
+from os.path import exists, splitext
+
+
+env.hosts = ['54.87.180.223', '52.23.245.155']
+
+
+@task
+def do_deploy(archive_path):
+    """ Function that deply archive"""
+
+    # Checking if path exists
+    if not exists(archive_path):
+        return False
+    # Spliting the files
+    arch_name = archive_path.split('/')[1]
+    file_name = splitext(arch_name)[0]
+    tmp_path = "/tmp/{}".format(arch_name)
+    data_path = "/data/web_static/releases/{}".format(file_name)
+    # Fabric commands
+    put(archive_path, '/tmp/')
+    run('mkdir -p {}'.format(data_path))
+    run('tar -xzf {}  -C {}'.format(tmp_path, data_path))
+    run('rm {}'.format(tmp_path))
+    run('mv {}/web_static/* {}/'.format(data_path, data_path))
+    run('rm -rf {}/web_static'.format(data_path))
+    run('rm -rf /data/web_static/current')
+    run('ln -s {} /data/web_static/current'.format(data_path))
+    print("New version deployed!")
+    return True
