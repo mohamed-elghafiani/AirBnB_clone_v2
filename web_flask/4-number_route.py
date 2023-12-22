d5084eac6f4321ddcf5051719e51bab81053b7d4
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-22 15:50:22 -0800

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """Home route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """hbnb route"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>")
+def c_route(text):
+    """c route"""
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_route(text="is cool"):
+    """python route"""
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>")
+def number_route(n):
+    """number route"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
