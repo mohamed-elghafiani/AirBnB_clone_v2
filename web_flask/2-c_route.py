4226f976700e89e815552e8a2f78ec1126ddf588
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-12-22 15:29:13 -0800

    route c

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """Home route"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    """hbnb route"""
+    return "HBNB"
+
+@app.route("/c/<string:text>")
+def c_route(text):
+    """c route"""
+    return text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
